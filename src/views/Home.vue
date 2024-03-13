cf2624e24463cf3309dda727454ef65f07ffebb1
Your Name <you@example.com>
2024-03-13 09:29:36 +0700

    change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,8 +19,11 @@
             <template v-slot:[`item.bnb_rate`]="{ item }">
               {{ formatPrice(usdt_price(item)) }}
             </template>
+            <template v-slot:[`item.rate`]="{ item }">
+              {{ formatPrice(usdt_rate/ratio(item)) }}
+            </template>
             <template v-slot:[`item.ratio`]="{ item }">
-              {{ ratio(item) }}
+              {{ formatPrice(ratio(item)) }}
             </template>
           </v-data-table>
         </v-col>
@@ -50,6 +53,7 @@ export default {
         { text: 'Token', value: 'token', sortable: false, },
         { text: 'GMO', value: 'gmo_rate', sortable: false, },
         { text: 'Binance', value: 'bnb_rate', sortable: false, },
+        { text: 'Rate', value: 'rate' },
         { text: 'Ratio', value: 'ratio' },
       ],
       gmo_list: [],
@@ -65,7 +69,7 @@ export default {
       this.getBinance()
       this.getGMO()
       this.getUsdtRate()
-    }, 5000);
+    }, 3000);
   },
   methods: {
     logout() {
@@ -76,26 +80,26 @@ export default {
       axios.get('https://api.chootc.com/api/p2p?type=sell&asset=usdt&fiat=vnd&page=1').then((res) => {
         this.usdt_rate = res.data.data[19].adv.price
       }).catch((err) => {
-        location.reload();
+        // location.reload();
       })
     },
     getGMO() {
       axios.get('https://api.chootc.com/api/gmo').then((res) => {
         this.gmo_list = []
         res.data.data.forEach(i => {
-        if(i.symbol.includes("JPY")){
+        if(i.symbol.includes("JPY") && !i.symbol.includes("LTC")){
           this.gmo_list.push({
             token: i.symbol ,
-            price: Number(i.last) - Number(i.last * 0.05 / 100)
+            price: Number(i.last) + Number(i.last * 0.1 / 100)
           })
         }
        });
       }).catch((err) => {
-        location.reload();
+        // location.reload();
       })
     },
     getBinance() {
-      let symbols = ["BTCUSDT", "ETHUSDT", "ETCUSDT", "XRPUSDT", "BCHUSDT", "QTUMUSDT", "BTGUSDT", "EOSUSDT", "ICXUSDT", "TRXUSDT", "ELFUSDT", "KNCUSDT", "GLMUSDT", "ZILUSDT", "WAXPUSDT", "POWRUSDT", "LRCUSDT", "STEEMUSDT", "STRAXUSDT", "ZRXUSDT", "SNTUSDT", "ADAUSDT", "CTXCUSDT", "BATUSDT", "THETAUSDT", "LOOMUSDT", "WAVESUSDT", "LINKUSDT", "ENJUSDT", "VETUSDT", "MTLUSDT", "IOSTUSDT", "QKCUSDT", "TFUELUSDT", "ANKRUSDT", "CHRUSDT", "MBLUSDT", "SXPUSDT", "COSUSDT", "HIVEUSDT", "ARPAUSDT", "CKBUSDT", "AERGOUSDT", "YFIUSDT", "UNIUSDT", "UMAUSDT", "COMPUSDT", "RSRUSDT", "AAVEUSDT", "RLCUSDT", "NMRUSDT", "BALUSDT", "RENUSDT", "SANDUSDT", "BELUSDT", "STPTUSDT", "GRTUSDT", "OXTUSDT", "LINAUSDT", "SNXUSDT", "CTSIUSDT", "PLAUSDT", "MANAUSDT", "LPTUSDT", "MKRUSDT", "CELRUSDT", "PUNDIXUSDT", "FRONTUSDT", "SUSHIUSDT", "OCEANUSDT", "COTIUSDT", "BNTUSDT", "CAKEUSDT", "XVSUSDT", "OGNUSDT", "ALICEUSDT", "CHZUSDT", "AXSUSDT", "DAIUSDT", "WOOUSDT", "ONTUSDT", "SHIBUSDT", "ONGUSDT", "MATICUSDT", "ALGOUSDT", "KLAYUSDT", "BNBUSDT", "SUNUSDT", "XLMUSDT", "XECUSDT", "AGIXUSDT", "SOLUSDT", "XTZUSDT", "JSTUSDT", "DOTUSDT", "ATOMUSDT", "CTKUSDT", "REIUSDT", "EGLDUSDT", "MASKUSDT", "CRVUSDT", "1INCHUSDT", "C98USDT", "DYDXUSDT", "FLOWUSDT", "BTTUSDT", "JASMYUSDT", "GALAUSDT", "REQUSDT", "AVAXUSDT", "GMTUSDT", "DARUSDT", "ACHUSDT", "SFPUSDT", "KSMUSDT", "TUSDT", "DOGEUSDT", "APTUSDT", "HOOKUSDT", "APEUSDT", "OPUSDT", "GMXUSDT", "STXUSDT", "CFXUSDT", "HFTUSDT", "ILVUSDT", "LEVERUSDT", "WLDUSDT", "SEIUSDT", "FXSUSDT", "IMXUSDT", "ARBUSDT", "RPLUSDT", "INJUSDT", "CELOUSDT", "FTMUSDT", "LDOUSDT", "FLOKIUSDT", "FETUSDT", "RNDRUSDT", "STGUSDT", "SUIUSDT", "MAVUSDT", "PEPEUSDT"]
+      let symbols = ["BTCUSDT", "ETHUSDT", "BCHUSDT", "XRPUSDT"]
       
       axios.get('https://api3.binance.com/api/v3/ticker/price', {
         params: {
@@ -110,12 +114,12 @@ export default {
           })
         })
       }).catch((err) => {
-        location.reload();
+        // location.reload();
       })
     },
     ratio(item) {
       let bnb_rate = this.binance_list.filter((i) => item.token.includes(i.token.slice(0,-4)))[0]
-      return bnb_rate ? this.formatPrice(this.usdt_rate/(item.price/ bnb_rate.price)) : 0
+      return bnb_rate ? this.usdt_rate/(item.price/ bnb_rate.price) : 0
     },
     usdt_price(item) {
       let token = this.binance_list.filter((i) => item.token.includes(i.token.slice(0,-4)))[0]
